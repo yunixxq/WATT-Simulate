7e62117f13d8b0c156abe646149f8a7f3a234732
Demian Vöhringer <demian.voehringer@fau.de>
2021-06-23 13:58:50 +0200

    static_page simulation added

---
+++ static_pages.py
@@ -0,0 +1,83 @@
+#!/bin/python3
+
+import matplotlib.pyplot as plt
+from scipy.stats import zipfian
+import time
+
+# x = random.zipf(a=2, size=1000)
+
+# print(x)
+
+def printTime(last):
+    post = time.time()
+    print(post-last)
+    return post
+def startTime():
+    return time.time()
+
+
+def simulate(discrete_elements, simulation_length, zipf_factor=2):
+    x = list(zipfian.rvs(zipf_factor, discrete_elements, size=simulation_length))
+    distribution = []
+    for element in range(1, discrete_elements+1):
+        counter = x.count(element)
+        distribution.append(counter)
+    return distribution
+
+writes_to_total_fraction = 0.3
+total_access = 1000
+
+total_pages = 100
+writes_to_total_pages = 0.4
+
+buffer_size = 20
+
+write_cost_factor = 1
+
+write_pages = int(total_pages * writes_to_total_pages)
+write_access = int(writes_to_total_fraction * total_access)
+
+read_pages = total_pages - write_pages
+read_access = total_access - write_access
+
+timer = startTime()
+
+write_distribution = simulate(write_pages, write_access, 0.5)
+timer = printTime(timer)
+read_distribution =  simulate(read_pages, read_access, 1.0)
+
+timer = printTime(timer)
+
+assert(sum(write_distribution + read_distribution) == total_access)
+assert(len(write_distribution + read_distribution) == total_pages)
+
+write_misses = []
+read_misses = []
+total_misses = []
+
+timer = printTime(timer)
+
+for read_buffer in range(0, buffer_size + 1):
+    read_miss = read_access - sum(read_distribution[0:read_buffer])
+    write_miss = write_access - sum(write_distribution[0:(buffer_size - read_buffer)])
+    read_miss += write_miss
+    read_misses.append(read_miss)
+    write_misses.append(write_miss)
+    total_misses.append(read_miss + write_miss * write_cost_factor)
+
+timer = printTime(timer)
+
+read_per_access = list(map(lambda x: x/total_access, read_misses))
+write_per_access = list(map(lambda x: x/total_access, write_misses))
+total_per_access = list(map(lambda x: x/total_access, total_misses))
+
+timer = printTime(timer)
+
+plt.plot(range(0, buffer_size + 1), read_per_access, label="Read_per_access")
+plt.plot(range(0, buffer_size + 1), write_per_access, label="Write_per_access")
+plt.plot(range(0, buffer_size + 1), total_per_access, label="Cost_per_access")
+plt.xlabel("Read_buffer_size")
+plt.ylabel("Cost per Access")
+plt.legend()
+
+plt.show()
