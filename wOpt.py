bb43f73a5fd1682d0de86724019e2cf3dcec4e09
Demian Vöhringer <demian.voehringer@fau.de>
2021-07-19 17:30:33 +0200

    Reduce amount of variables, reducing solving time + space

--- wOpt.py
+++ wOpt.py
@@ -3,6 +3,7 @@
 from os import write
 import gurobipy as gp
 from gurobipy import GRB, tupledict, tuplelist
+from numpy import number
 from scipy.stats import zipfian
 import pandas, random
 
@@ -16,16 +17,33 @@ def calcCost(accesses, is_write, ramsize, write_cost):
             print(len(is_write))
             assert(len(accesses) == len(is_write))
 
+        print("Building Tuplelist {} {}".format(len(pages), access_len))
+        def getValidTimesForPage(accesses: list, page: number):
+            first = accesses.index(page)
+            last = len(accesses) -1 - accesses[::-1].index(page)
+            return (first, last)
+        
+        start = {}
+        end = {}
+        for p in pages:
+            (first, last) = getValidTimesForPage(accesses, p)
+            start[p] = first
+            end[p] = last +1
 
-        pageTimes: tuplelist = gp.tuplelist([(p,t) for p in pages for t in range(-1,access_len+1)])
+
+
+        pageTimes: tuplelist = gp.tuplelist([(p,t) for p in pages for t in range(start[p]-1, end[p] +1)])
+        print("Building model")
 
         model = gp.Model("writeOpt")
 
+        print("Adding Variables")
         ram: tupledict = model.addVars(pageTimes, name="ram", vtype=GRB.BINARY)
         dirty: tupledict = model.addVars(pageTimes, name="dirty", vtype=GRB.BINARY)
         delta_ram: tupledict = model.addVars(pageTimes, name="delta_ram", vtype=GRB.BINARY)
         delta_dirty: tupledict = model.addVars(pageTimes, name="delta_dirty", vtype=GRB.BINARY)
 
+        print("Adding constraints")
         #  $\sum_t p_{s,t} \leq P$ = Puffergröße
         model.addConstrs((ram.sum('*', time) <= ramsize for time in range(-1, access_len +1)), "capacity")
 
@@ -36,16 +54,16 @@ def calcCost(accesses, is_write, ramsize, write_cost):
         model.addConstrs((dirty[pageTime] <= ram[pageTime] for pageTime in pageTimes), "dirtyInRam")
 
         # $p_{s,t} \leq p_{s,t-1} + \delta p_{s,t}$ = Seite kann nur durch lesen eingelagert werden.
-        model.addConstrs((ram[p, t] <= ram[p, t-1] + delta_ram[p, t] for p in pages for t in range(access_len +1)), "readFresh");
+        model.addConstrs((ram[p, t] <= ram[p, t-1] + delta_ram[p, t] for p in pages for t in range(start[p], end[p] +1)), "readFresh");
 
         # $d_{s,t} \leq d_{s,t+1} + \delta d_{s,t}$ = Eine Seite, verliert ihr dirty flag nur, wenn sie geschrieben wird
-        model.addConstrs((dirty[p, t] <= dirty[p, t+1] + delta_dirty[p, t] for p in pages for t in range(-1, access_len)), "writeDirty");
+        model.addConstrs((dirty[p, t] <= dirty[p, t+1] + delta_dirty[p, t] for p in pages for t in range(start[p] -1, end[p])), "writeDirty");
 
         #  - $p_{s,0} = 0$ = am anfang ist der Puffer leer
-        model.addConstrs((ram[page, -1] == 0 for page in pages), name="emptyStart")
+        model.addConstrs((ram[page, start[page]-1] == 0 for page in pages), name="emptyStart")
 
         # Optional: $d_{s,t_{max}} = 0$ =Am ende ist der Puffer Sauber
-        model.addConstrs((dirty[page, access_len] == 0 for page in pages), name="cleanStop")
+        model.addConstrs((dirty[page, end[page]] == 0 for page in pages), name="cleanStop")
 
         # $p_{s,t}\geq 1$ wenn gelesen, $\geq 0$ sonst
         model.addConstrs((ram[(p,t)] == 1 for t,p in enumerate(accesses)), name="read")
@@ -55,6 +73,7 @@ def calcCost(accesses, is_write, ramsize, write_cost):
 
         model.update()
 
+        print("Adding Objective")
         # $\min \sum_{s,t} (\delta d_{s,t} \cdot \alpha + \delta p_{s,t})$
         if write_cost != 0:
             model.setObjective(delta_ram.sum() + write_cost * delta_dirty.sum(), GRB.MINIMIZE)
@@ -62,6 +81,7 @@ def calcCost(accesses, is_write, ramsize, write_cost):
             model.setObjectiveN(delta_ram.sum(), 0, priority=1)
             model.setObjectiveN(delta_dirty.sum(), 1, priority=0)
 
+        print("Optimizing")
         model.optimize()
 
         pagesInRam = []
@@ -123,7 +143,7 @@ csv_name = "wopt.csv"
 try:
     print("try read from cache")
     run_file(csv_name, ramsize, write_cost)
-except:
+except FileNotFoundError:
     print("No CSV with name \"{}\" found".format(csv_name))
     max_page=100
     length = 2000
