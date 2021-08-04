ae174c2e77463592ffebd84399f31d8f1da7cd12
Demian Vöhringer <demian.voehringer@fau.de>
2021-08-04 12:02:44 +0200

    FullRun:
    - refactoring

--- fullRun.py
+++ fullRun.py
@@ -1,31 +1,39 @@
 #!/bin/python3
 
-from evalAccessTable import do_run as generateStandards
+from evalAccessTable import plotGraph, oneFullRun as generateStandards
 from optW.generateOpt import run_file as generateModel
 from optW.solveOpt import evalModelMulti
-import pandas
+import pandas, os
 
-def run_all(writeCost):
-    file = "./accesses_10000_3000_40_0.5_1.0.csv"
-    read_file = "./ALL_heatUp_0/Datareads.csv"
-    write_file = "./ALL_heatUp_0/Datawrites.csv"
-    generateStandards(file)
+def run_all(directory, writeCost):
+    directory = directory + "_" + str(writeCost)
+    try:
+        os.mkdir(directory)
+    except:
+        pass
+
+    file = "./test.csv"
+    csv_start = directory + "/Data"
+    read_file = csv_start + "reads.csv"
+    write_file = csv_start + "writes.csv"
+    generateStandards(file, csv_start, writeCost)
     generateModel(file)
     
     df_reads = pandas.read_csv(read_file)
     df_writes = pandas.read_csv(write_file)
     xList = list(df_reads["X"])
 
-    (reads, writes) = evalModelMulti([writeCost], xList)
+    (reads, writes) = evalModelMulti([writeCost], reversed(xList))
     print(reads)
     print(writes)
-    df_reads["wOpt"] = reads
-    df_writes["wOpt"] = writes
-    df_reads.to_csv(read_file)
-    df_writes.to_csv(write_file)
-    
+    df_reads["wOpt"] = list(reversed(reads))
+    df_writes["wOpt"] = list(reversed(writes))
+    df_reads.to_csv(read_file, index=False)
+    df_writes.to_csv(write_file, index=False)
+    plotGraph(csv_start, writeCost)
+
 
 
 
 if __name__ == "__main__":
-    run_all(8)
+    run_all("./evalFullRun", 1)
