108ff203e9ccb49c79068338ff5959ec47219b25
Demian Vöhringer <demian.voehringer@fau.de>
2021-09-16 15:23:54 +0200

    change zipf_trace config

--- generate_traces.py
+++ generate_traces.py
@@ -23,10 +23,13 @@ def generate_zipf_distribution(access_list):
 
 
 def generate_zipf_file(total_pages, write_pages, write_access, write_zipf, read_pages, read_access, read_zipf, shuffle, csv_name):
+    print("Generating Writes")
     write_accesses = simulate_zipf(write_pages, write_access, write_zipf, shuffle, pages=total_pages)
+    print("Generating Reads")
     read_accesses = simulate_zipf(read_pages, read_access, read_zipf, shuffle, pages=total_pages)
     reads = [(x, False) for x in read_accesses]
     writes = [(x, True) for x in write_accesses]
+    print("Shuffeling Accesses")
     total = random.sample(reads + writes, len(reads+writes))
     (pages, is_write) = zip(*total)
     d2 = {'pages': pages, "is_write": is_write}
@@ -34,4 +37,17 @@ def generate_zipf_file(total_pages, write_pages, write_access, write_zipf, read_
     df2.to_csv(csv_name, index=False)
 
 if __name__ == "__main__":
-    generate_zipf_file(100, 40, 4000, 0.5, 60, 6000, 1.0, True, "zipf_accesses.csv")
+    pages_total = 1000
+    pages_w_fract = 0.4
+    pages_r_fract = 1-pages_w_fract
+    access_total = 5000
+    access_w_fract = 0.4
+    access_r_fract = 1-access_w_fract
+    zipf_w = 0.5
+    zipf_r = 1.0
+    
+    generate_zipf_file(
+        pages_total,
+        int(pages_total*pages_w_fract), int(access_total*access_w_fract), zipf_w,
+        int(pages_total*pages_r_fract), int(access_total*access_r_fract), zipf_r,
+        True, "zipf_accesses.csv")
