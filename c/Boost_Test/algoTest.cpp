1f48eb23568383101f82e7e632adf415dca5240b
Demian Vöhringer <demian.voehringer@fau.de>
2021-11-09 17:55:05 +0100

    add aditional tests

--- c/Boost_Test/algoTest.cpp
+++ c/Boost_Test/algoTest.cpp
@@ -18,37 +18,86 @@ EvalAccessTable init(){
 }
 
 template<class algo>
-bool compareTwoAlgos(EvalAccessTable& instance, std::vector<uInt>& reads, std::vector<uInt>& writes, std::string name, std::vector<int> args = {}){
+bool compareToOther(EvalAccessTable& instance, std::string other, std::string name, std::vector<int> args = {}){
     instance.runAlgorithm<algo>(name, args);
-    return (instance.getReads(name) == reads) && (instance.getWrites(name) == writes);
+    return (instance.getReads(name) == instance.getReads(other)) && (instance.getWrites(name) == instance.getWrites(other));
 
+}
+
+template<class algo>
+void runAlgo(EvalAccessTable& instance, std::string name, std::vector<int> args = {}){
+    instance.runAlgorithm<algo>(name, args);
 }
 
 BOOST_AUTO_TEST_SUITE(compare_algo)
     BOOST_AUTO_TEST_CASE(lru) {
-        EvalAccessTable test = init();
+        EvalAccessTable instance = init();
 
-        std::vector<uInt> lru_reads, lru_writes;
-        lru_reads = test.getReads("lru");
-        lru_writes = test.getWrites("lru");
-
-        BOOST_TEST(compareTwoAlgos<LRU>(test, lru_reads, lru_writes, "lru_0"));
-        BOOST_TEST(compareTwoAlgos<LRU1>(test, lru_reads, lru_writes, "lru_1"));
-        BOOST_TEST(compareTwoAlgos<LRU2>(test, lru_reads, lru_writes, "lru_2"));
-        BOOST_TEST(compareTwoAlgos<LRU2a>(test, lru_reads, lru_writes, "lru_2a"));
-        BOOST_TEST(compareTwoAlgos<LRU2b>(test, lru_reads, lru_writes, "lru_2b"));
-        BOOST_TEST(compareTwoAlgos<LRU3>(test, lru_reads, lru_writes, "lru_3"));
+        BOOST_TEST(compareToOther<LRU>(instance, "lru", "lru_0"));
+        BOOST_TEST(compareToOther<LRU1>(instance, "lru", "lru_1"));
+        BOOST_TEST(compareToOther<LRU2>(instance, "lru", "lru_2"));
+        BOOST_TEST(compareToOther<LRU2a>(instance, "lru", "lru_2a"));
+        BOOST_TEST(compareToOther<LRU2b>(instance, "lru", "lru_2b"));
+        BOOST_TEST(compareToOther<LRU3>(instance, "lru", "lru_3"));
     }
-    BOOST_AUTO_TEST_CASE(lru_oter) {
-        EvalAccessTable test = init();
+    BOOST_AUTO_TEST_CASE(lru_sim) {
+        EvalAccessTable instance = init();
 
-        std::vector<uInt> lru_reads, lru_writes;
-        lru_reads = test.getReads("lru");
-        lru_writes = test.getWrites("lru");
-        BOOST_TEST(compareTwoAlgos<LRU_K>(test, lru_reads, lru_writes, "lru_K_0", {1, 0}));
-        BOOST_TEST(compareTwoAlgos<LRU_K_alt>(test, lru_reads, lru_writes, "lru_K2_0", {1}));
-        // BOOST_TEST(compareTwoAlgos<LFU_K<>>(test, lru_reads, lru_writes, "lfu_K_0", {1}));
-        // BOOST_TEST(compareTwoAlgos<LFU_K_Z<>>(test, lru_reads, lru_writes, "lfu_K_0", {1, 0}));
+        BOOST_TEST(compareToOther<LRU_K_Z>(instance, "lru", "lru_K1_Z0", {1, 0}));
+        BOOST_TEST(compareToOther<LRU_K_Z>(instance, "lru", "lru_K1_Z10", {1, 10}));
+        BOOST_TEST(compareToOther<LRU_K_Z>(instance, "lru", "lru_K1_Z-10", {1, -10}));
+        BOOST_TEST(compareToOther<LRU_K_alt>(instance, "lru", "lru_K_ALT_1", {1}));
+
+        BOOST_TEST(compareToOther<LFU_K<>>(instance, "lru", "lfu_K_1", {1}));
+        BOOST_TEST(compareToOther<LFU_K_Z<>>(instance, "lru", "lfu_K1_Z0", {1, 0}));
+        BOOST_TEST(compareToOther<LFU_K_Z<>>(instance, "lru", "lfu_K1_Z10", {1, 10}));
+        BOOST_TEST(compareToOther<LFU_K_Z<>>(instance, "lru", "lfu_K1_Z-10", {1, -10}));
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lru", "lfu2_K1_Z0", {1, 0}));
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lru", "lfu2_K1_Z10", {1, 10}));
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lru", "lfu2_K1_Z-10", {1, -10}));
+        BOOST_TEST(compareToOther<LFU_K_alt<>>(instance, "lru", "lfu_K_ALT_1", {1, -10}));
+        BOOST_TEST(compareToOther<LFU_K_Z_D1<>>(instance, "lru", "lfu_K1_Z0_D10", {1, 0, 10}));
+        BOOST_TEST(compareToOther<LFU_K_Z_D1<>>(instance, "lru", "lfu_K1_Z10_D10", {1, 10, 10}));
 
     }
+    BOOST_AUTO_TEST_CASE(lru_ks) {
+        EvalAccessTable instance = init();
+        runAlgo<LRU_K_Z>(instance, "lru_K1_Z0", {1,0});
+        BOOST_TEST(compareToOther<LRU_K_alt>(instance, "lru_K1_Z0", "lru_K_ALT_1", {1}));
+        runAlgo<LRU_K_Z>(instance, "lru_K2_Z0", {2,0});
+        BOOST_TEST(compareToOther<LRU_K_alt>(instance, "lru_K2_Z0", "lru_K_ALT_2", {2}));
+        runAlgo<LRU_K_Z>(instance, "lru_K10_Z0", {10,0});
+        BOOST_TEST(compareToOther<LRU_K_alt>(instance, "lru_K10_Z0", "lru_K_ALT_10", {10}));
+    }
+    BOOST_AUTO_TEST_CASE(lfu_ks) {
+        EvalAccessTable instance = init();
+        // InRam History
+        runAlgo<LFU_K<>>(instance, "lfu_K1", {1});
+        BOOST_TEST(compareToOther<LFU_K_Z<>>(instance, "lfu_K1", "lfu_K1_Z0", {1, 0}));
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K1", "lfu2_K1_Z0", {1, 0}));
+        runAlgo<LFU_K<>>(instance, "lfu_K2", {2});
+        BOOST_TEST(compareToOther<LFU_K_Z<>>(instance, "lfu_K2", "lfu_K2_Z0", {2, 0}));
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K2", "lfu2_K2_Z0", {2, 0}));
+        runAlgo<LFU_K<>>(instance, "lfu_K10", {10});
+        BOOST_TEST(compareToOther<LFU_K_Z<>>(instance, "lfu_K10", "lfu_K10_Z0", {10, 0}));
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K10", "lfu2_K10_Z0", {10, 0}));
+
+        // Out of Ram History
+        runAlgo<LFU_K_Z<>>(instance, "lfu_K1_Z1", {1, 1});
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K1_Z1", "lfu2_K1_Z1", {1, 1}));
+
+        runAlgo<LFU_K_Z<>>(instance, "lfu_K1_Z10", {1, 10});
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K1_Z10", "lfu2_K1_Z10", {1, 10}));
+
+        runAlgo<LFU_K_Z<>>(instance, "lfu_K2_Z10", {2, 10});
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K2_Z10", "lfu2_K2_Z10", {2, 10}));
+
+        runAlgo<LFU_K_Z<>>(instance, "lfu_K2_Z-10", {2, -10});
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K2_Z-10", "lfu2_K2_Z-10", {2, -10}));
+
+        runAlgo<LFU_K_Z<>>(instance, "lfu_K10_Z10", {10, 10});
+        BOOST_TEST(compareToOther<LFU_K_Z2<>>(instance, "lfu_K10_Z10", "lfu2_K10_Z10", {10, 10}));
+        BOOST_TEST(compareToOther<LFU_K_Z_D1<>>(instance, "lfu_K10_Z10", "lfu_K10_Z10_D10", {10, 10, 10}));
+ }
+
 BOOST_AUTO_TEST_SUITE_END()
