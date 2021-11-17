78ecac66e63aa62c8d9d850cc178231a3e553ecf
Demian Vöhringer <demian.voehringer@fau.de>
2021-11-17 14:13:27 +0100

    main now works with argv

--- c/evalAccessTable/main.cpp
+++ c/evalAccessTable/main.cpp
@@ -4,7 +4,15 @@
 
 #include "evalAccessTable.hpp"
 
-int main(int, char **) {
-    EvalAccessTable eval("./../tpcc_64_-5.csv", "./out/");
-    return 0;
+int main(int argc, char *argv[]) {
+    if(argc == 2){
+        EvalAccessTable eval(argv[1], "./out/");
+        return 0;
+    }else if(argc == 3){
+        EvalAccessTable eval(argv[1], argv[2]);
+        return 0;
+    }else {
+        std::cout << "Usage: " << argv[0] << "tracefile [outdir]" <<std::endl;
+	return 1;
+    }
 }
