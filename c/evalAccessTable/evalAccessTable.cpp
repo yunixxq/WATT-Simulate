2b84ec21cc9261064acb7f2d233369677837cd67
Demian Vöhringer <demian.voehringer@fau.de>
2021-12-01 11:42:15 +0100

    add a few more run configurations

--- c/evalAccessTable/evalAccessTable.cpp
+++ c/evalAccessTable/evalAccessTable.cpp
@@ -42,7 +42,7 @@ void EvalAccessTable::runFromFilename(bool only_new, bool ignore_old, bool full_
     runAlgorithm("lean30", Lean_Generator(30));
     runAlgorithm("lean40", Lean_Generator(40));
 
-    for(int k: {20, 2, 1}){
+    for(int k: {32, 16, 8, 4, 2, 1}){
         for(int z: {-1}){
             runAlgorithm("lfu_k_realF_e" + std::to_string(k), LRU_2K_E_real_Generator(8, 4, k, 5, 5, false, 0));
             runAlgorithm("lfu_k" + std::to_string(k) + "_z" + std::to_string(z), LFU_K_Z_Generator(k, z));
