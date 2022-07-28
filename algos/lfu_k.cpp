f8ebae8ceca1b71577126e6780befcfaf885b6a6
Demian Vöhringer <demian.voehringer@fau.de>
2022-07-28 15:59:03 +0200

    make getFrequency simpler

--- algos/lfu_k.cpp
+++ algos/lfu_k.cpp
@@ -4,29 +4,18 @@
 
 #include "lfu_k.hpp"
 
-double get_frequency(std::vector<RefTime>& candidate, RefTime curr_time, bool ignore_first ){
-     int value = 10;
-    long best_age = 100000, best_value = 0;
-    if(candidate.size() == 1){
-        best_age = curr_time - candidate[0];
-        best_value = 10;
-    }
-    value += 10;
-    uint pos = 0;
-    if(ignore_first)
-        pos++;
-    for(; pos < candidate.size(); pos++){
-        long age = curr_time - candidate[pos];
-        long left = value*best_age;
-        long right = best_value * age;
-        if(left> right){
-            best_value = value;
-            best_age = age;
-        }
-        value+=10;
-    }
-    best_age = std::max(1L, best_age);
-    return best_value * 1.0 /best_age;
+float minn(float a, float b) { return a<b ? a : b; }
+float maxx(float a, float b) { return a>b ? a : b; }
+
+using namespace std;
+
+double get_frequency(std::vector<RefTime>& array, RefTime now, bool ignore_first) {
+   float best = 0;
+   uint i = 0;
+   if(ignore_first) i++;
+   for (; i<array.size(); i++)
+      best = maxx(best, minn((float)(i+1) / (now-array[i]), 1.0));
+   return best;
 }
 
 double get_frequency(std::vector<std::pair<RefTime, bool>>& candidate, RefTime curr_time, [[maybe_unused]] uint write_cost){
